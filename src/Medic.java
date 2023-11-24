2089fe4427de9cb875bd553d7163bcbb789e7566
BolotbekovvB <149691592+BolotbekovvB@users.noreply.github.com>
2023-11-25 00:38:32 +0600

    Medic.java

--- src/Medic.java
+++ src/Medic.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Medic {
+public class Medic extends Hero {
+    int healthPoints = 50;
+    @Override
+    public void applySuperAbility() {
+        System.out.println("Medic применил лечение");
+    }
+    public void increaseExperience(){
+        healthPoints = (healthPoints / 10) + healthPoints;
+        System.out.println("У Medic увеличен опыт");
+    }
 }
